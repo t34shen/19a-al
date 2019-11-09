536ae3724168d4f53f4deaab109b33d802f92f73
t34shen <t34_shen@163.com>
2019-11-09 20:00:38 +0800

    modified version after lab class 11.8

--- lab1/merge_sort.c
+++ lab1/merge_sort.c
@@ -3,20 +3,19 @@
 #include <math.h>
 #include <time.h>
 #include <windows.h>
-#define times 1000000
-
+#define times 20000000
 
 /*得到以order为幂次的数据*/
 //乱序排法
-void randomArray(char arr[], int t){
+void randomArray(int arr[], int t){
     srand((unsigned)time(0));
     for(int i = 0; i < times; i++){
-        arr[i] = (rand() % 250);
+        arr[i] = (rand() % 1000);
         //printf("%d ", i);
     }
 }
 //顺序排法
-void orderArray(char arr[], int t){
+void orderArray(int arr[], int t){
     arr[0] = -127;
     for(int i = 1; i < times; i++){
         if(arr[i-1] >= 124){
@@ -28,7 +27,7 @@ void orderArray(char arr[], int t){
     return arr;
 }
 //逆序排法
-void reverseArray(char arr[], int t){
+void reverseArray(int arr[], int t){
     arr[0] = 127;
     for(int i = 1; i < times; i++){
         if(arr[i-1] <= -124){
@@ -41,7 +40,7 @@ void reverseArray(char arr[], int t){
 }
 
 //内部函数merge
-void merge(char a[], char b[], int low, int mid, int high){
+void merge(int a[], int b[], int low, int mid, int high){
     //b从k个开始放东西
     int i = low;
     int k = low;
@@ -68,12 +67,19 @@ void merge(char a[], char b[], int low, int mid, int high){
         k++;
         i++;
     }
+
     for(int i = low; i < high + 1; i++){
         a[i] = b[i];
+//        printf("%d\t", a[i]);
     }
+//    printf("\n\n");
+//    for(int j = 0; j < times; j++){
+//        printf("%d\t", a[j]);
+//    }
+//    printf("\n\n");
 }
 //recursion sort
-void mergeSortRe(char a1[], char b1[], int low, int high){
+void mergeSortRe(int a1[], int b1[], int low, int high){
     if(high - low > 0){
         //printf("分解： %d %d %d\n", low, (low+high)/2, high);
 
@@ -84,86 +90,41 @@ void mergeSortRe(char a1[], char b1[], int low, int high){
     }
 }
 
-////loop-version
-//int middle(int i, int j){
-//    return (i + j) / 2;
-//}
-//
-//void mergeSort(char a[], char b[], int length){
-//    //得到有多少层的mergesort
-//    int hei = log2(length) + 1;
-//
-//    //构造一个存放分组关键字的数组
-//    int nodecount = pow(2, hei + 1) - 1;
-//    int node[nodecount];
-//
-//    //将每一层的值对应关键字放进这个数组
-//    int count = 0;
-//    node[0] = middle(0, length - 1);
-//    count += 1;
-//    for(int i = 0; i < hei; i++){
-//        for(int j = 0; j < (int)pow(2, i); j++){
-//            if(j = 0){
-//                node[(int)pow(2, i + 1) - 1 + j] =
-//                middle(0, node[(int)pow(2, i) - 1]);
-//            }else if(j = pow(2, i) - 1){
-//                node[(int)pow(2, i + 1) - 1 + j] =
-//                middle(node[(int)pow(2, i + 1) - 2], length);
-//            }else if(j % 2 == 1){
-//                node[(int)pow(2, i + 1) - 1 + j] =
-//                middle(node[(int)pow(2, i) - 1 + j / 2], node[(int)pow(2, i - 1) - 1 + j / 4]);
-//            }
-//        }
-//    }
-//    for(int i = 0; i < nodecount; i++){
-//        printf(node[i]);
-//    }
-//    //利用关键字从底层开始查找，merge
-//
-//    //得到结果，输出
-//
-//}
-
 int middle(int i, int j){
     return (i + j) / 2;
 }
 
-void mergeSort(char a[], char b[], int length){
-    //计算有多少层，记得有一共是有
+void mergeSort(int a[], int b[], int length){
     int hei = log2(length) + 1;
     //printf("层数%d\n", hei);
     for(int i = 1; i < hei ; i++){
-        printf("这都第 %d 层了！\n", i);
-        //计算buf，buf * 2即为该层每次sort的量
+        //printf("这都第 %d 层了！\n", hei - i);
+        //计算buf，buf * 2即为该层每次sort的量s
         int buf = (int)pow(2, i);
         //printf("buf: %d\n", buf);
-        for(int j = 0; j <= length / buf - 1; j++){
+        for(int j = 0; j < length / buf; j++){
             //printf("%d %d %d\n", j * buf, middle(j * buf, (j+1) * buf - 1), (j+1) * buf - 1);
             merge(a, b, j * buf, middle(j * buf, (j+1) * buf - 1), (j+1) * buf - 1);
-            //如果传递到了末尾，则merge剩余的量
-            if((length % buf > buf / 2) && (j == length / buf - 1)){
-                merge(a, b, j * length / buf, (j+1/2) * length / buf, length - 1);
+            //如果传递到了末尾，则merge剩余s的量
+            if(((length % buf) > (buf / 2)) && (j == length / buf - 1)){
+                //printf("surprise!\n");
+                merge(a, b, buf * (j + 1), (j + 1) * buf + buf / 2 - 1, length - 1);
                 //printf("%d %d %d\n", j * length / buf, (j+1/2) * length / buf, length - 1);
-            }else if(i == hei - 1){
-                merge(a, b , 0, length - (length % buf) - 1, length - 1);
-                //printf("%d %d %d\n", 0, length - (length % buf) - 1, length - 1);
             }
+        }
+        //靠，这一块复杂了，bug不断。。。
+        if(i == hei - 1){
+            merge(a, b , 0, buf-1, length - 1);
         }
     }
 }
 
-//const int order = 6;
-//const int times = pow(10, order);
-//times = times * 2;
-//printf("times: %d\n", times);
-//int times = 100000000;
-char arr[times];
-char result[times];
+int arr[times];
+int result[times];
 
 int main(void){
     clock_t start, finish;
     double duration;
-
 
     //printf("get there");
     reverseArray(arr, times);
