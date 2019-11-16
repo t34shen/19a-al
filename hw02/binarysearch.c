3a40b367806793c37860975abf76d732dba710cc
t34shen <t34_shen@163,com>
2019-11-16 20:39:17 +0800

    hw2 11.16 version

--- hw02/binarysearch.c
+++ hw02/binarysearch.c
@@ -3,12 +3,14 @@
 
 int main(void){
     //test
+    /*give a reverse order array
+    */
     int arr[N];
     for (int i = 0; i < N; i++){
         arr[i] = N - i;
     }
-    int num = 0;
-    int pin = binaryRe(arr, num, 0, N-1);
+    int num = 1;
+    int pin = binary(arr, num, 0, N-1);
     if(pin > -1){
         printf("the number %d is on the position of: %d", num, pin + 1);
     }else{
@@ -17,14 +19,14 @@ int main(void){
     return 0;
 }
 
-int binaryRe(int array[], int num, int low, int high){
+int binaryReWithNoOrder(int array[], int num, int low, int high){
     int r;
     if(array[(low+high)/2] != num){
         if(high == low){;
             return -1;
         }else{
-            int r1 = binaryRe(array, num, low, (low+high)/2);
-            int r2 = binaryRe(array, num, (low+high)/2 + 1, high);
+            int r1 = binaryReWithNoOrder(array, num, low, (low+high)/2);
+            int r2 = binaryReWithNoOrder(array, num, (low+high)/2 + 1, high);
             if(r1 < r2){
                 return r2;
             }else if(r2 < r1){
@@ -38,7 +40,29 @@ int binaryRe(int array[], int num, int low, int high){
     }
 }
 
-int binary(int array[], int num){
+int binaryRe(int array[], int num, int low, int high){
+    int r;
+    if(array[(low+high)/2] == num){
+        return (low+high)/2;
+    }else if(array[(low+high)/2] < num && low != high){
+        return binaryRe(array, num, low, (low+high)/2);
+    }else if(array[(low+high)/2] > num && low != high){
+        return binaryRe(array, num, (low+high)/2+1, high);
+    }else{
+        return -1;
+    }
+}
 
-    return 0;
+int binary(int array[], int num, int low, int high){
+    while(array[(low+high)/2] != num){
+        int v = array[(low+high)/2];
+        if(high == low){
+            return -1;
+        }else if(v < num){
+            high = (low+high)/2;
+        }else if(v > num){
+            low = (low+high)/2 + 1;
+        }
+    }
+    return (low+high)/2;
 }
