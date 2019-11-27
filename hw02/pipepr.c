4280857e42b4f10832f97bd1054da0cb09a7b4aa
t34shen <t34_shen@163.com>
2019-11-27 23:21:33 +0800

    almost finish, still with bug

--- hw02/pipepr.c
+++ hw02/pipepr.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 #define N 20
 /*估计要改造一下快速排序了 
-*毕竟查找中位数目前不知道有啥更好的方法
-*难道要用线性时间的排序排完了然后找中位数 
+ *毕竟查找中位数目前不知道有啥更好的方法
+ *难道要用线性时间的排序排完了然后找中位数 
+ *然鹅发现并没有什么好改造的。。。 
 */
-int findmiddle(int arr[]){
-	//
+int findmiddle(int array[], int k, int low, int high){
+    //use the lowest element as the base
+    int base = low;
+    int basevalue = array[low];
+    int li = low + 1;
+    int ri = high;
+    //quicksearch loop
+    while (li < ri){
+        for(;ri >= base ; ri--){
+            if(array[ri] < basevalue){
+                array[base] = array[ri];
+                base = ri;
+                break;
+            }
+        }
+        for(;li <= base ; li++){
+            if(array[li] > basevalue){
+                array[base] = array[li];
+                base = li;
+                break;
+            }
+        }
+    }
+    array[base] = basevalue;
+//    
+//    for (int i = 0; i < N; i++){
+//        printf("%d\t", array[i]);
+//    }
+
+    if (base == k){
+        return basevalue;
+    }else if (base > k){
+        return findmiddle(array, k, low, base);
+    }else if (base < k){
+	    return findmiddle(array, k, base + 1, high); 
+    }
 }
 
 void randomArray(int arr[]){
@@ -27,6 +62,6 @@ int main(void){
 	int oily[N];
 	randomArray(oily);
 	printarray(oily);
-	int middle = findmiddle(oily);
+	int middle = findmiddle(oily, N/2, 0, N-1);
 	printf("The Y coordinate of oil pipe is %d\n", middle);
 } 
--- hw02/radixsort.c
+++ hw02/radixsort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define N 20
 #define NN 399
 
@@ -6,25 +7,104 @@
 *然后在桶内正用计数排序，
 *如果是比较插入的话最差情况下恐怕不能做到 O(n) 
 */ 
+
+//realize queue
+typedef struct QUEUE{
+	int data[N];
+	int front;
+	int rear;
+	int size;
+}queue;
+
+queue* creatQueue(){
+	queue* q = (queue*)malloc(sizeof(queue));
+	q->front = -1;
+	q->rear = -1;
+	q->size = 0;
+	return q;
+}
+void addQ(queue* q, int item){
+	q->rear++;
+	q->size++;
+	q->data[q->rear] = item;
+}
+int deleteQ(queue* q){
+	q->front++;
+	q->size--;
+	return q->data[q->front];
+}
+
+void countsort(queue* q){
+	int si = q->size;
+	if(si > 1){
+		int t[N];
+		for(int i=0; i < N; i++){
+		    t[i] = 0;
+		}
+		for(int j=0; j < si; j++){
+		    t[q->data[j] % N]++;
+		}
+		for(int k=1; k < N; k++){
+			t[k] = t[k] + t[k-1];
+		}
+		int s[q->size];
+		for(int n = si; n > 0; n--){
+			s[t[q->data[n] % N] - 1] = q->data[n];
+			t[q->data[n] % N]--;
+		}
+		for(int i = 0; i < si; i++){
+			q->data[i] = s[i];
+		}
+	}else if(si == 1){
+		if(q->data[0] > q->data[1]){
+			int tem = q->data[1];
+			q->data[1] = q->data[0];
+			q->data[0] = tem;
+		}
+	}
+}
+
 //sort function
 void radixsort(int arr[]){
-	
+	queue* q[N];
+	for(int k=0; k < N; k++){
+		q[k] = creatQueue();
+	}
+	for(int i=0; i < N; i++){
+		int tem = arr[i] / N;
+		addQ(q[tem], arr[i]);
+	}
+	for(int i = 0; i< N; i++){
+		countsort(q[i]);
+	}
+	int whole[N];
+	int k = 0;
+	for(int i=0; i < N; i++){
+		for(int j = 0; i < q[i]->size; j++){s
+		    whole[k] = q[i]->data[j];
+			k++; 
+		} 
+	}
+	for(int i = 0; i < N; i++){
+	    arr[i] = whole[i]; 
+	} 
 }
 
 //get random value array
 void randomArray(int arr[]){
     for(int i = 0; i < N; i++){
-        arr[i] = rand() % 100;
+        arr[i] = rand() % 399;
     }
 }
 
 void printarray(int arr[]){
-	printf("The array is: ");
+	printf("The array is:  ");
 	for (int i = 0; i < N; i++){
 		printf("%d\t", arr[i]);
 	}
 	printf("\n");
 }
+
 int main(void){
 	//test
 	int array[N];
