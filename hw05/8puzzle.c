f6c6df8993c61e0fb85b1e6bfae3a99ba2d52cee
t34shen <t34_shen@163.com>
2019-12-25 21:47:57 +0800

    confusing! 8puzzle

--- hw05/8puzzle.c
+++ hw05/8puzzle.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 
 /* global values */
+/* store the possible steps' x,y coordinate */
 int puzzlexy[5][2];
 
 /* initiate the 8-puzzle
  * in a 3-3 array
  */
-int puzzle[3][3] = {{0, 5, 4}, {3, 2, 8}, {7, 1, 4}};
+int puzzle[3][3] = {{6, 1, 0}, {3, 2, 5}, {7, 8, 4}};
 int correct[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 int puzzleCopy[3][3];
 
@@ -35,6 +39,14 @@ void find0(int p[][3]){
 	}
 }
 
+/* change puzzle according to the step x,y*/
+void step(int p[][3], int x, int y){
+	int xp = puzzlexy[0][0];
+	int yp = puzzlexy[0][1];
+	p[xp][yp] = p[x][y];
+	p[x][y] = 0;
+}
+
 /* print the spare one */
 void print0(int p[][3]){
 	find0(p);
@@ -42,31 +54,107 @@ void print0(int p[][3]){
 			puzzlexy[0][0], puzzlexy[0][1]);
 }
 
-/* find the directions puzzle can go
- * give the possible directions in array dir
- * return the number of direction 
+/* copy puzzle from p to copy*/
+void pCopy(int p[][3], int copy[][3]){
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			copy[i][j] = p[i][j];
+		}
+	}
+}
+
+/* find the directions puzzle can go, give the possible directions
+ * return the number of direction
+ * # warning # this function must be implement after 'find0'
  */
-int findDir(int p[][3], int dir[][3]){
-	
-	return 0;
+int findDir(int p[][3]){
+	/* possible steps number */
+	int count;
+	/* get the coodinate of spare blank*/
+	int x = puzzlexy[0][0];
+	int y = puzzlexy[0][1];
+	/*calculate the possible step*/
+	if(x == 1){
+		if(y == 1){
+			count = 4;
+			puzzlexy[1][0] = 1; puzzlexy[1][1] = 0;
+			puzzlexy[2][0] = 1; puzzlexy[2][1] = 2;
+			puzzlexy[3][0] = 0; puzzlexy[3][1] = 1;
+			puzzlexy[4][0] = 2; puzzlexy[4][1] = 1;
+		}else{
+			count = 3;
+			puzzlexy[1][0] = 1; puzzlexy[1][1] = 1;
+			puzzlexy[2][0] = 0; puzzlexy[2][1] = y;
+			puzzlexy[3][0] = 2; puzzlexy[3][1] = y;
+		}
+	}else{
+		if(y == 1){
+			count = 3;
+			puzzlexy[1][0] = 1; puzzlexy[1][1] = 1;
+			puzzlexy[2][0] = x; puzzlexy[2][1] = 0;
+			puzzlexy[3][0] = x; puzzlexy[3][1] = 2;
+		}else{
+			count = 2;
+			puzzlexy[1][0] = x; puzzlexy[1][1] = 1;
+			puzzlexy[2][0] = 1; puzzlexy[2][1] = y;
+		}
+	}
+	return count;
 }
 
 /* return the number of the same blocks */
 int correctness(int p[][3]){
-	
-	return 0;
+	int correct = 0;
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			//once i forgot to put 3* before i
+			int n = p[i][j];
+			int x = n / 3;
+			int y = n % 3;
+			correct += abs(x-i + y-j);
+		}
+	}
+	return correct;
 }
 
 /**/
 void astar(int p[][3]){
-	
-	return;
+	int x, y;
+	while(correctness(p) != 0){
+		find0(p);
+		int num = findDir(p);
+		/* find the best way to go, with puzzleCopy*/
+		int bestway = -1;
+		int best = correctness(p);
+		int temp;
+		for(int i = 1; i < 1+num; i++){
+			pCopy(p, puzzleCopy);
+			step(puzzleCopy, puzzlexy[i][0], puzzlexy[i][1]);
+			temp = correctness(puzzleCopy);
+			if(temp < best){
+				best = temp;
+				bestway = i;
+			}
+		}
+		if(bestway == -1){
+			bestway = 1 + rand() % num;
+		}
+		printf("best: %d\n", best);
+		/* give the best way to puzzle*/
+		x = puzzlexy[bestway][0];
+		y = puzzlexy[bestway][1];
+		step(p, x, y);
+		
+		printP(p);
+	}
 }
 
 int main(void){
 	//some test
-	printP(puzzle);
-	printP(correct);
-	print0(puzzle);
-	print0(correct);
-} 
+//	printP(puzzle);
+//	printP(correct);
+//	print0(puzzle);
+//	print0(correct);
+	astar(puzzle);
+}
+
